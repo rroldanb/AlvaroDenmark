1af36ff4ee376e7948d77c38af9fdde1a8fee5cd
Ruben Roldan <ruben.roldan.b@gmail.com>
2024-05-08 00:31:21 -0400

    agrega whatsapp y logos am-cm ajustes espaciados y colores

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		fontFamily: {
-			sans: ['Gotham SSm A, sans-serif']
+			sans: ['sans-serif']
 		},
 		extend: {},
 	},
